e61e12761aa095714b84bf84e5d0cdfe88a30693
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2024-02-29 14:21:57 -0700

    Began styling the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import videoBg from "../resources/videos/cooking-bg.mp4";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="page">
+      <section className="title-section">
+        <video src={videoBg} autoPlay loop muted />
+        <div className="title-cover"></div>
+        <div className="title-content">
+          <h1>Your Personal Chef of Phoenix, AZ</h1>
+          <p>Bringing the delicacies of the kitchen to your home and table</p>
+        </div>
+      </section>
     </div>
   );
 };

14e2610b333412d9a40fb6807eebb53042ff8e6c
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2024-03-05 15:52:59 -0700

    Implemented PageTitleVideo component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,17 @@
 import React from "react";
+import PageTitleVideo from "../components/PageTitleVideo";
 import videoBg from "../resources/videos/cooking-bg.mp4";
 
 const Home = () => {
   return (
     <div className="page">
-      <section className="title-section">
-        <video src={videoBg} autoPlay loop muted />
-        <div className="title-cover"></div>
-        <div className="title-content">
-          <h1>Christopher Smith - Personal Chef</h1>
-          <p>
-            An accomplished and fervent chef, proudly rooted in Phoenix, AZ,
-            dedicated to delivering culinary excellence straight to your home
-            and table.
-          </p>
-        </div>
-      </section>
+      <PageTitleVideo
+        asset={videoBg}
+        title={"Christopher Smith - Personal Chef"}
+        description={
+          "An accomplished and fervent chef, proudly rooted in Phoenix, AZ, dedicated to delivering culinary excellence straight to your home and table."
+        }
+      />
     </div>
   );
 };

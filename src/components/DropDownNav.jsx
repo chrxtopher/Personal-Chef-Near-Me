69b523dc4d054f4f670b0af195aa0fba3098d742
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2024-03-08 13:41:20 -0700

    DropDownNav now accepts click handlers
    This is to close the drop down menu after selecting an option

--- src/components/DropDownNav.jsx
+++ src/components/DropDownNav.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/dropDownNav.css";
 
-const DropDownNav = ({ linkObjectsArray, active }) => {
+const DropDownNav = ({ linkObjectsArray, active, clickHandler }) => {
   return (
     <div className={`drop-down-nav ${active ? "active" : "inactive"}`}>
       <ul>
         {linkObjectsArray.map((linkObj) => {
           return (
             <li>
-              <Link to={`${linkObj.route}`}>{`${linkObj.btnName}`}</Link>
+              <Link
+                to={`${linkObj.route}`}
+                onClick={clickHandler}
+              >{`${linkObj.btnName}`}</Link>
             </li>
           );
         })}

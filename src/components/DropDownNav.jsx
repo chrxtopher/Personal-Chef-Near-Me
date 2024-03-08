b248b7e35e2f0f3d1bd5cd5aadf8e7ed1fe0b534
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2024-03-08 13:25:28 -0700

    Created and styled DropDownNav menu

---
+++ src/components/DropDownNav.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/dropDownNav.css";
+
+const DropDownNav = ({ linkObjectsArray, active }) => {
+  return (
+    <div className={`drop-down-nav ${active ? "active" : "inactive"}`}>
+      <ul>
+        {linkObjectsArray.map((linkObj) => {
+          return (
+            <li>
+              <Link to={`${linkObj.route}`}>{`${linkObj.btnName}`}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default DropDownNav;

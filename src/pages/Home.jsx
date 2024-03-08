94023429279a9e98a18f6813244bf0659e01fc55
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2024-03-08 09:20:05 -0700

    Began building home page with filler text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,6 +12,29 @@ const Home = () => {
           "An accomplished and fervent chef, proudly rooted in Phoenix, AZ, dedicated to delivering culinary excellence straight to your home and table."
         }
       />
+      <section className="page-info">
+        <h2>Placeholder Title</h2>
+        <hr />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
+          voluptatem omnis atque inventore, commodi nesciunt a rem id et
+          eligendi ipsum molestias! Aspernatur ratione id soluta molestiae harum
+          libero beatae!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta ut
+          omnis quidem quis dolor, accusamus eligendi esse, nam cumque numquam
+          incidunt animi doloremque nesciunt nostrum, magnam distinctio laborum
+          rerum officiis!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Non omnis
+          nisi odio excepturi ipsa ducimus dicta voluptatem commodi! Doloremque
+          maiores eaque quidem dignissimos in iure ex exercitationem distinctio,
+          eos mollitia.
+        </p>
+        <hr />
+      </section>
     </div>
   );
 };

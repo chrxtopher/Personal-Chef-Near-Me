479885cd941b75a055465d6e1b999e448fb8cf45
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2024-03-15 16:29:11 -0700

    Styling and further set up for BookingForm
    Implementation will be set up very soon

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import "../styles/bookingForm.css";
 
 const BookingForm = () => {
   const [service, setService] = useState("Private Event");
@@ -11,62 +12,115 @@ const BookingForm = () => {
   return (
     <div className="form-container">
       <form className="booking-form">
-        <label for="fname">First Name</label>
-        <input type="text" id="fname" name="fname" />
-        <label for="lname">Last Name</label>
-        <input type="text" id="lname" name="lname" />
-        <label for="email">Email</label>
-        <input type="email" id="email" name="email" />
-        <label for="phone">Phone Number</label>
-        <input
-          type="tel"
-          id="phone"
-          name="phone"
-          placeholder="123-456-7890"
-          pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-        />
-        <small>Format: 123-456-7890</small>
-        <label for="service">Requested Service</label>
-        <select id="service" name="service" onChange={handleServiceChange}>
-          <option value="Private Event">Private Event</option>
-          <option value="Meal Prep">Meal Prep</option>
-        </select>
-        <label for="date">Date of Service</label>
-        <input type="date" id="date" name="date" />
-        {service === "Private Event" && (
-          <div>
-            <label for="guests">Number of Guests:</label>
+        <div className="input-container">
+          <div className="input-section">
+            <label for="fname">First Name</label>
             <input
-              type="number"
-              id="guests"
-              name="guests"
-              min="2"
-              max="6"
-              defaultValue="2"
+              type="text"
+              id="fname"
+              name="fname"
+              placeholder="John"
+              required
             />
-            <small>2-6</small>
-            <label for="description">Event Description</label>
-            <textarea name="description" rows="10" cols="50" />
-            <label for="allergies">Allergy Concerns & Preferences</label>
-            <textarea name="allergies" rows="10" cols="50" />
+          </div>
+          <div className="input-section">
+            <label for="lname">Last Name</label>
+            <input
+              type="text"
+              id="lname"
+              name="lname"
+              placeholder="Smith"
+              required
+            />
+          </div>
+        </div>
+        <div className="input-container">
+          <div className="input-section">
+            <label for="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="john.smith@example.com"
+              required
+            />
+          </div>
+          <div className="input-section">
+            <label for="phone">Phone Number</label>
+            <input
+              type="tel"
+              id="phone"
+              name="phone"
+              placeholder="123-456-7890"
+              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+              required
+            />
+            <small>Format: 123-456-7890</small>
+          </div>
+        </div>
+        <div className="input-container">
+          <div className="input-section">
+            <label for="service">Requested Service</label>
+            <select id="service" name="service" onChange={handleServiceChange}>
+              <option value="Private Event">Private Event</option>
+              <option value="Meal Prep">Meal Prep</option>
+            </select>
+          </div>
+          <div className="input-section">
+            <label for="date">Date of Service</label>
+            <input type="date" id="date" name="date" required />
+          </div>
+        </div>
+
+        {service === "Private Event" && (
+          <div className="input-container">
+            <div className="input-section">
+              <label for="guests">Guest Count</label>
+              <select id="guests" name="guests">
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+              </select>
+            </div>
           </div>
         )}
         {service === "Meal Prep" && (
-          <div>
-            <label for="mealCount">Number of Meals Per Week</label>
-            <select id="mealCount" name="mealCount">
-              <option value="10">10</option>
-              <option value="20">20</option>
-              <option value="30">30</option>
-              <option value="40">40</option>
-              <option value="50+">50+</option>
-            </select>
-            <label for="description">Meal Description</label>
-            <textarea name="description" rows="10" cols="50" />
-            <label for="allergies">Allergy Concerns & Preferences</label>
-            <textarea name="allergies" rows="10" cols="50" />
+          <div className="input-container">
+            <div className="input-section">
+              <label for="mealCount">Number of Meals Per Week</label>
+              <select id="mealCount" name="mealCount">
+                <option value="10">10</option>
+                <option value="20">20</option>
+                <option value="30">30</option>
+                <option value="40">40</option>
+                <option value="50+">50+</option>
+              </select>
+            </div>
           </div>
         )}
+        <div className="input-container">
+          <div className="input-section large-text">
+            <label for="description">Event & Food Description</label>
+
+            <textarea name="description" rows="10" cols="50" />
+            <small>
+              Basis of expectations for menu and services. Please provide some
+              brief information on the type of event (if applicable), preferred
+              ingredients for plates/meals (ie. proteins and veggies), or any
+              information that will help us curate the perfect experience.
+            </small>
+          </div>
+          <div className="input-section large-text">
+            <label for="allergies">Allergy Concerns & Preferences</label>
+            <textarea name="allergies" rows="10" cols="50" />
+            <small>
+              Please be sure to include any allergy information or preferences
+              for all guests that I will be cooking for.
+            </small>
+          </div>
+        </div>
       </form>
     </div>
   );

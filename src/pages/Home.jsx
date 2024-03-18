b51804b2fa2d18a5b74422da3b0bfb38304d6987
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2024-03-17 22:13:07 -0700

    Added new text and photos to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,12 @@ import videoBg from "../assets/videos/cooking-bg.mp4";
 import HowItWorks from "../components/HowItWorks";
 import ChefChris from "../components/ChefChris";
 import DuoImage from "../components/DuoImage";
+import NavButton from "../components/NavButton";
 import entree1 from "../assets/photos/entree_1.png";
+import entree2 from "../assets/photos/entree_2.jpeg";
 import entree3 from "../assets/photos/entree_3.jpeg";
+import entree4 from "../assets/photos/entree_4.jpeg";
+
 const Home = () => {
   return (
     <div className="page">
@@ -20,25 +24,32 @@ const Home = () => {
         <h2>Private Chef Services In Phoenix, AZ</h2>
         <hr />
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
-          voluptatem omnis atque inventore, commodi nesciunt a rem id et
-          eligendi ipsum molestias! Aspernatur ratione id soluta molestiae harum
-          libero beatae!
+          Embark on a culinary adventure with an exclusive private dining event.
+          Whether you're planning an intimate date night for two or a
+          sophisticated business dinner, I am dedicated to crafting a menu that
+          exceeds your expectations. Immerse yourself in an atmosphere of
+          elegance as you savor each dish, knowing that every element has been
+          thoughtfully curated to ensure an exceptional dining experience.
         </p>
+        <NavButton
+          route={"/privateevents"}
+          title={"Discover Your Next Dining Event"}
+        />
         <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta ut
-          omnis quidem quis dolor, accusamus eligendi esse, nam cumque numquam
-          incidunt animi doloremque nesciunt nostrum, magnam distinctio laborum
-          rerum officiis!
+          Experience the ultimate solution to busy schedules and hectic
+          lifestyles with my convenient meal prep service. Say goodbye to the
+          stress of meal planning, grocery shopping, and cooking, and hello to
+          delicious, chef-crafted meals. Whether you're a health-conscious
+          individual, a busy professional, or simply seeking convenience without
+          sacrificing taste, I can deliver a wide variety of nutritious and
+          flavorful options to suit your needs. From wholesome breakfasts to
+          satisfying lunches and delectable dinners, each meal is expertly
+          prepared.
         </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Non omnis
-          nisi odio excepturi ipsa ducimus dicta voluptatem commodi! Doloremque
-          maiores eaque quidem dignissimos in iure ex exercitationem distinctio,
-          eos mollitia.
-        </p>
+        <NavButton route={"/mealprep"} title={"Learn More About Meal Prep"} />
       </section>
       <DuoImage asset1={entree1} asset2={entree3} />
+      <DuoImage asset1={entree2} asset2={entree4} />
       <HowItWorks />
       <section className="page-info">
         <hr />

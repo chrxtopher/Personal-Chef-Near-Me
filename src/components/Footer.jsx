95649a74bdfc3f0d79df6500ac38eb7426f1f42e
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2024-10-24 16:39:55 -0700

    redundant content has been adjusted

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,10 +9,7 @@ const Footer = () => {
       <img src={PCNM_LOGO} alt="PCNM Logo" />
       <div>
         <h1>Personal Chef Near Me</h1>
-        <p>
-          An accomplished and fervent chef, proudly rooted in Arizona, dedicated
-          to delivering culinary excellence straight to your home and table.
-        </p>
+        <p>Check out my Facebook page with the button below!</p>
         <a
           href="https://www.facebook.com/people/Personal-Chef-Near-Me/61555784183482/"
           className="icon-link"

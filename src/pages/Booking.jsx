d3e80327db0a9f9cf8e97be6580296edbb880eb3
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2025-05-01 08:52:23 -0700

    Text adjustments

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -5,16 +5,15 @@ const Booking = () => {
   return (
     <div className="page">
       <section className="page-info">
+        <p></p>
         <h1>Let's Get Started</h1>
+        <p></p>
         <p>
           Complete and submit the form below to begin the booking process. I
           will receive your inquiry information and will be in contact via email
           and/or phone call soon. Event and food descriptions do not need to
           include every detail, as we will cover all specifications during our
           phone appointment and consultation.
-        </p>
-        <p>
-          Please double check that all contact information provided is correct.
         </p>
         <hr />
       </section>
